08075b3865e9c7ab57d978af7d1c89be2d70a56f
prasad <vprasad.java@gmail.com>
2018-01-02 04:30:49 -0500

    Testing static code analysis

--- src/main/java/varaprasad/prasad/App.java
+++ src/main/java/varaprasad/prasad/App.java
@@ -5,7 +5,7 @@ package varaprasad.prasad;
  *
  */
 public class App {
-    public static void main(String[] args){
+    public static void main(final String[] args) {
     System.out.println("Hello World!");
     }
 }
